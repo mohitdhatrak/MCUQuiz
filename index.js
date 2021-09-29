b66e4d821f8bad047817f858692d876968701426
Mohit Dhatrak <91209576+mohitdhatrak@users.noreply.github.com>
2021-09-29 14:55:04 +0530

    added 5 more questions

--- index.js
+++ index.js
@@ -24,29 +24,54 @@ function quiz(question, options, answer) {
 
 var questionBank = [
   {
-    question: "Who is Black Panther’s sister? ",
+    question: "Q.1) Who is Black Panther’s sister? ",
     options: ["Ramonda","Shuri","Nakia","Okoye"],
     answer: 2
   }, 
   {
-    question: "What were the three items Rocket claims he needs in order to escape the prison? ",
+    question: "Q.2) What were the three items Rocket claims he needs in order to escape the prison? ",
     options: ["A security card, a fork, and a prosthetic leg","A security band, a battery, and a prosthetic leg","A pair of binoculars, a detonator, and a prosthetic leg","A knife, cable wires, and a prothestic leg"],    
     answer: 2
   }, 
   {
-    question: "What is the name of the little boy Tony befriends while stranded in the Iron Man 3? ",
+    question: "Q.3) What is the name of the little boy Tony befriends while stranded in the Iron Man 3? ",
     options: ["Harry","Henry","Harley","Holden"],
     answer: 3
   },
   {
-    question: "The Flerkens who are a race of extremely dangerous aliens, resembles what? ",
+    question: "Q.4) The Flerkens who are a race of extremely dangerous aliens, resembles what? ",
     options: ["Cats","Ducks","Racoons","Snakes"],
     answer: 1
   },
   {
-    question: "What type of doctor is Stephen Strange ",
+    question: "Q.5) What type of doctor is Stephen Strange? ",
     options: ["Cardiothoracic Surgeon","Trauma Surgeon","Plastic Surgeon","Neurosurgeon"],    
     answer: 4 
+  },
+  {
+    question: "Q.6) What fake name does Natasha use when she first meets Tony? ",
+    options: ["Nicole Romanoff","Natalie Rushman","Natalia Romanoff","Naya Rabe"],
+    answer: 2
+  },
+  {
+    question: "Q.7) What does the Winter Soldier say after Steve recognizes him for the first time? ",
+    options: ["Who the hell is Bucky?","What did you say?","What!","I'm not bucky!"],
+    answer: 1
+  },
+  {
+    question: "Q.8) Who was the last holder of the Tesseract before Thanos claims it for his Infinity Gauntlet? ",
+    options: ["Thor","Loki","The Collector","Ebony Maw"],
+    answer: 4
+  },
+  {
+    question: "Q.9) In the Incredible Hulk, what does Tony tell Thaddeus Ross at the end of the film? ",
+    options: ["That he wants to study The Hulk","That he knows about S.H.I.E.L.D.","That they are putting a team together","That Thaddeus owes him money"],
+    answer: 3
+  },
+  {
+    question: "Q.10) What year was the first Iron Man movie released, kicking off the Marvel Cinematic Universe? ",
+    options: ["2005","2008","2012","2010"],
+    answer: 2
   }
 ];
 
@@ -54,17 +79,17 @@ for(var i = 0; i < questionBank.length; i++) {
   quiz(questionBank[i].question, questionBank[i].options, questionBank[i].answer); 
 }
 
-console.log(chalk.magenta("Your Final score is", score + " out of 5"));
+console.log(chalk.magenta("Your Final score is", score + " out of 10"));
 console.log("  ");
 console.log(chalk.cyan("Here's the Leaderboard - ")); 
 var leaderBoard = [
   {
     name: "Rohit ",
-    score: 4
+    score: 9
   },
   {
     name: "Sunil ",
-    score: 3
+    score: 5
   },
   {
     name: "Mahesh Deshmukh ",
